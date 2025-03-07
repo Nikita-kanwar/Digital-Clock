813ed38f7e1a2f614e146a5e11f8bd6982e1d7a8
Nikita Kanwar <kanwarnikita2025@gmail.com>
2025-03-07 13:24:48 +0530

    Get data in clock function

--- script.js
+++ script.js
@@ -9,4 +9,10 @@ function clock(){
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
     let ampm = ho >= 12 ?"PM":"AM"; 
+
+    hours.innerHTML = ho;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+    period.innerHTML = ampm;
 };
+clock();
